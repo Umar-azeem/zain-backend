d02d11d6a802e5b9ca0a72a150da5fcd8099cd71
Umar-azeem <umara4436@gmail.com>
2025-09-03 18:12:46 +0500

    abd_db

--- models/Product.js
+++ models/Product.js
@@ -14,4 +14,4 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Product", productSchema, "abd_db");
+module.exports = mongoose.model("abd_db", productSchema,);
