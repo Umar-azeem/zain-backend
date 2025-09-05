35b0b761f8733ece4fd7f2de1556029f3d414174
Umar-azeem <umara4436@gmail.com>
2025-09-05 21:06:43 +0500

    fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
+
+// dotenv.config();
+//  const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -16,37 +20,32 @@ app.use(
     credentials: true,
   })
 );
-
 // ✅ Parse JSON
 app.use(express.json());
 
 // ✅ Routes
 app.use("/api/products", productRoutes);
 
+// app.listen(5000, () => {
+//   console.log(`Example app listening on port 5000`);
+// });
 // ✅ MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
-
-// ✅ Vercel requires export
-module.exports = app;
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (!mongoose.connection.readyState) {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+}
 
 // ✅ Vercel requires: export handler
-// module.exports = app;
-//  app.listen(PORT, () => {
-//    console.log(`✅ Server http://localhost:${PORT} par chal raha hai`);
-//  });
+module.exports = app;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server http://localhost:${PORT} par chal raha hai`);
+// });
+
+
+
+
+
+
+
